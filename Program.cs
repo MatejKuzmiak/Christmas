b619174c0c6e44f951fdec166070f51c6d0636b7
Matej Kuzmiak <matejkuzmiak@gmail.com>
2021-12-20 21:42:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merry Christmas \n");
+
+            int i, j, k;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 4; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (k = 1; k <= (2 * i - 1); k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("   | |");
+
+            Console.ReadKey();
+        }
+    }
+}
